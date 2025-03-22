803c9a255b61abe9502dd5ff21bd9fdce9fb4369
reizergrzegorz <reizergrzegorz@gmail.com>
2025-03-22 19:46:26 +0100

    refactor form using form_with

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,23 @@
 class UsersController < ApplicationController
 
-    def new 
-      end
+  def new 
+    @user = User.new
+  end
     
-    def create
-      @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-      # @user = User.new(user_params)
+  def create
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
       
-      if @user.save
-        redirect_to new_user_path
-      else
-        render :new, status: :unprocessable_entity
-      end
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
     
-    private
+  private
 
-    def user_params
-      params.require(:user).permit(:username, :email, :password)
-    end
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
