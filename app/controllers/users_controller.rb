2ba3be07f51ae44f9d34117dba3130c610901235
reizergrzegorz <reizergrzegorz@gmail.com>
2025-03-22 20:15:18 +0100

    add edit and update method to User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,21 @@ class UsersController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
-    
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+        redirect_to new_user_path(@user)
+    else
+        render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def user_params
